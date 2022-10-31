481191bca5f64c57da853d0e8b3cb0fcb40a7a68
jeka0 <latosh02@mail.ru>
2022-10-31 15:51:21 +0300

    added routing

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const PORT = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('This is GET reqest')
 })
 
-app.listen(3000)
+app.post('/', function (req, res) {
+  res.send('This is POST reqest')
+})
+
+app.delete('/', function (req, res) {
+  res.send('This is DELETE reqest')
+})
+
+app.put('/', function (req, res) {
+  res.send('This is PUT reqest')
+})
+
+app.listen(PORT,()=>console.log(`The server is running on a port ${PORT}...`))
